422daa33992ebb4c52b1585c58fa6c43386fdba7
Jeff Lee <jhlsuper@gmail.com>
2020-07-07 22:03:27 +0900

    desktop

---
+++ react-project/reactfrondend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="PostingSection">
+
+        <h2>대나무 숲 글 작성하기</h2>
+        <input />
+        <textarea />
+        
+      </div>
+
+      <div className="ViewSection">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
