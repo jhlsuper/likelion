a65e54ec33c8dd647680b75a4729bfcaf6da89a0
jhlsuper <jhlsuper@gmail.com>
2020-07-22 16:03:55 +0900

    serializer class done

---
+++ 새 폴더/firstrest/post/serializer.py
@@ -0,0 +1,9 @@
+from .models import Post
+from rest_framework import serializers
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        #filels = '__all__' 밑에거랑 같게 나옴
+        fields= ['id','title','body']
+        read_only_fiels =('title',)
